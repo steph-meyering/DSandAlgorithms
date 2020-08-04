0cc1a2d4cfaeb99655beab5907101fdf15c140ed
Steph Meyering <stephane.meyering@gmail.com>
2020-08-04 12:28:01 -0700

    add LeetCode problem solution

--- leetcode/637.py
+++ leetcode/637.py
@@ -27,3 +27,23 @@ class Solution:
             total, div = pair
             res[i] = total / div
         return res
+
+# RECURSIVE:
+
+# class Solution:
+#     def averageOfLevels(self, root: TreeNode) -> List[float]:
+#         res = []
+#         self.helper(root, res, 0)
+#         for i, pair in enumerate(res):
+#             total, div = pair
+#             res[i] = total / div
+#         return res
+
+#     def helper(self, node, res, depth):
+#         if node:
+#             if len(res) == depth:
+#                 res.append([0, 0])
+#             res[depth][0] += node.val
+#             res[depth][1] += 1
+#             self.helper(node.left, res, depth + 1)
+#             self.helper(node.right, res, depth + 1)
