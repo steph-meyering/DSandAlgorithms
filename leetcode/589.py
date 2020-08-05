d119ae3dbea6a122d474ec872e1f35d48fe3450e
Steph Meyering <stephane.meyering@gmail.com>
2020-08-05 10:51:26 -0700

    add LeetCode problem solution

--- leetcode/589.py
+++ leetcode/589.py
@@ -1,11 +1,28 @@
+# Recursive
+
+# class Solution:
+#     def preorder(self, root: 'Node') -> List[int]:
+#         res = []
+#         self.helper(root, res)
+#         return res
+
+#     def helper(self, root, res):
+#         if root:
+#             res.append(root.val)
+#             for node in root.children:
+#                 self.helper(node, res)
+
+# Iterative
+
 class Solution:
     def preorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        stack = [root]
         res = []
-        self.helper(root, res)
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            for child in reversed(node.children):
+                stack.append(child)
         return res
-
-    def helper(self, root, res):
-        if root:
-            res.append(root.val)
-            for node in root.children:
-                self.helper(node, res)
