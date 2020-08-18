40e620b5703f1cdf6d311d9c62013402e58e067a
Steph Meyering <stephane.meyering@gmail.com>
2020-08-17 18:56:48 -0700

    add LeetCode problem solution

---
+++ leetcode/563.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTilt(self, root: TreeNode) -> int:
+        self.tilt = 0
+        self.dfs(root)
+        return self.tilt
+
+    def dfs(self, root):
+        if not root:
+            return 0
+        left = self.dfs(root.left)
+        right = self.dfs(root.right)
+        self.tilt += abs(left - right)
+        return left + right + root.val
