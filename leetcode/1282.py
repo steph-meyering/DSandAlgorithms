d585094138a627d85f27b3a3aa81897ca8328816
Steph Meyering <stephane.meyering@gmail.com>
2020-10-08 18:04:16 -0700

    add LeetCode problem solution

---
+++ leetcode/1282.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        groups = {}
+        res = []
+        for i, size in enumerate(groupSizes):
+            if size not in groups:
+                groups[size] = []
+            groups[size].append(i)
+            if len(groups[size]) == size:
+                res.append(groups[size])
+                groups[size] = []
+        return res
