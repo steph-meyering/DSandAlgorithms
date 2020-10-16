58e6ad90fb88921767c6e4dc7e9dab538579608c
Steph Meyering <stephane.meyering@gmail.com>
2020-10-15 17:11:07 -0700

    add LeetCode problem solution

---
+++ leetcode/868.py
@@ -0,0 +1,12 @@
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        b = bin(n)
+        last_1, res = None, 0
+        for i in range(2,len(b)):
+            if last_1:
+                if b[i] == "1":
+                    res = max(res, i - last_1)
+                    last_1 = i
+            elif b[i] == "1":
+                last_1 = i
+        return res
