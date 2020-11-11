a001c8a8a6e50886319187d8c1297f9d2d7eef3c
Steph Meyering <stephane.meyering@gmail.com>
2020-11-10 22:06:16 -0800

    add LeetCode problem solution

---
+++ leetcode/113.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: TreeNode, s: int) -> List[List[int]]:
+        self.res = []
+        self.dfs(root, s, [])
+        return self.res
+
+    def dfs(self, root, s, path):
+        if root:
+            path.append(root.val)
+            if not root.left and not root.right and s == root.val:
+                return self.res.append(path)
+            self.dfs(root.left, s - root.val, path[:])
+            self.dfs(root.right, s - root.val, path[:])
