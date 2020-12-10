a2a78b55fdbab68543b94328d9742cbf5621eaf7
Steph Meyering <stephane.meyering@gmail.com>
2020-12-09 23:07:00 -0800

    add LeetCode problem solution

--- leetcode/122.py
+++ leetcode/122.py
@@ -1,5 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        res = 0
+        for i in range(1, len(prices)):
+            res += max(prices[i] - prices[i-1], 0)
+        return res
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         max_prof = 0
         for i in range(1, len(prices)):
             max_prof = max(max_prof, prices[i] - prices[i-1] + max_prof)
