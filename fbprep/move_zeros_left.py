5d5942d731ba4479c3c199b08f56ac5c0da7a1d3
Steph Meyering <stephane.meyering@gmail.com>
2020-12-11 13:42:12 -0800

    add first fb pb

---
+++ fbprep/move_zeros_left.py
@@ -0,0 +1,10 @@
+def move_zeros_to_left(A):
+    offset = 0
+    for i in range(len(A)-1, -1, -1):
+        if A[i] == 0:
+            offset += 1
+        else:
+            A[i + offset] = A[i]
+    for i in range(offset):
+        A[i] = 0
+    return A
