db3aa1dc5c0750ca0ceae8bb455339ad2324359e
Steph Meyering <stephane.meyering@gmail.com>
2020-12-18 12:33:41 -0800

    add LeetCode problem solution

--- leetcode/334.py
+++ leetcode/334.py
@@ -1,5 +1,18 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
+        a, b = float('inf'), float('inf')
+        for num in nums:
+            if num <= a:
+                a = num
+            elif num <= b:
+                b = num
+            else:
+                return True
+        return False
+
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
         l = len(nums)
         for i in range(l-2):
             for j in range(i+1, l-1):
