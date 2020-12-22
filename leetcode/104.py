a11b3d92e27e137feb4177f95c1b9cb730b500f4
Steph Meyering <stephane.meyering@gmail.com>
2020-12-22 11:26:49 -0800

    add LeetCode problem solution

--- leetcode/104.py
+++ leetcode/104.py
@@ -1,4 +1,6 @@
-# Definition for a binary tree node.
+# Iterative solution
+#
+# # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
@@ -19,3 +21,21 @@ class Solution:
                     q.append(n.right)
             d += 1
         return d
+
+# Recursive solution
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return max(left, right) + 1
