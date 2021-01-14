df1ddc689e69655c78846dae7d41c3ccdb638c19
Steph Meyering <stephane.meyering@gmail.com>
2021-01-14 11:10:17 -0800

    add LeetCode problem solution

--- leetcode/701.py
+++ leetcode/701.py
@@ -1,10 +1,23 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return TreeNode(val)
+        if root.val < val:
+            root.right = self.insertIntoBST(root.right, val)
+        elif val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        return root
+
+
+class Solution2:
     def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
         # compare root val to insert val
         # go down left or right
