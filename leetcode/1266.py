6db7376f586e32ed73c9c88af9c091f7deb4d8f9
Steph Meyering <stephane.meyering@gmail.com>
2021-01-25 11:55:14 -0800

    add LeetCode problem solution

--- leetcode/1266.py
+++ leetcode/1266.py
@@ -11,3 +11,13 @@ class Solution:
         x1, y1 = A
         x2, y2 = B
         return max(abs(x1-x2), abs(y1-y2))
+
+
+class Solution2:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        res = 0
+        for i in range(len(points)-1):
+            x1, y1 = points[i]
+            x2, y2 = points[i+1]
+            res += max(abs(x1-x2), abs(y1-y2))
+        return res
