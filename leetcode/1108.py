86102714750a9f575f94b7a783191d820fd7ccea
Steph Meyering <stephane.meyering@gmail.com>
2021-04-16 16:32:29 -0700

    add LeetCode problem solution

--- leetcode/1108.py
+++ leetcode/1108.py
@@ -1,3 +1,14 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
         return re.sub(r'\.','[.]', address)
+
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        # return re.sub(r'\.','[.]', address)
+        res = ""
+        for char in address:
+            if char == ".":
+                res += '[.]'
+            else:
+                res += char
+        return res
